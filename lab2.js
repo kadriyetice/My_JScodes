de75ad049ba6f14cd6d565732435fe8cb38e0943
kadriyetice <kadriyetice@gmail.com>
2018-08-11 20:55:43 -0500

    My Second Commit

---
+++ lab2.js
@@ -0,0 +1,22 @@
+//var num1,num2,temp;
+//num1=50;
+//num2=60;
+//
+//
+//temp=num2;
+//num2=num1;
+//num1=temp;
+//console.log("Num1 value is "+num1);
+//console.log("Num2 value is "+num2);
+
+
+//var gallonAmount,literAmount;
+//gallonAmount=parseInt(prompt("Enter the amount of gallon"));
+//literAmount=gallonAmount*3.78541
+//console.log(gallonAmount+" gallons is equal to "+literAmount+" litters");
+
+//var name,age,birthYear;
+//name=prompt("Enter your name");
+//birthYear=parseInt(prompt("What is your birthyear?"));
+//age=2018-birthYear;
+//console.log("Hello "+name+" ! Based on your input,your age is "+age+" :)")
